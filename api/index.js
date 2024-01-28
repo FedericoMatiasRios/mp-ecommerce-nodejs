6349a9a14d273f27d22e2d8352395481eb8dc10a
frios <frios@jst.gob.ar>
2024-01-28 20:13:09 -0300

    .

--- api/index.js
+++ api/index.js
@@ -1,29 +1,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
-//const { MercadoPagoConfig, Preference } = require('mercadopago');
-const mercadopago = require("mercadopago");
-
+const { MercadoPagoConfig, Preference } = require('mercadopago');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-/* const client = new MercadoPagoConfig({
-  accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
-  options: {
-      integratorId: 'dev_24c65fb163bf11ea96500242ac130004',
-  },
-}); */
-
-mercadopago.configure({
-  access_token: process.env.MERCADOPAGO_ACCESS_TOKEN,
-  "x-integrator_id": "dev_24c65fb163bf11ea96500242ac130004",
-  integrator_id: 'dev_24c65fb163bf11ea96500242ac130004'
-});
-
-mercadopago.configurations.setAccessToken(process.env.MERCADOPAGO_ACCESS_TOKEN);
-
+const client = new MercadoPagoConfig({
+    accessToken: process.env.MERCADOPAGO_ACCESS_TOKEN,
+    integratorId: 'dev_24c65fb163bf11ea96500242ac130004',
+  });
 
 // Use body-parser middleware
 app.use(bodyParser.json());
@@ -51,8 +38,74 @@ app.get('/detail', function (req, res) {
 
         console.log(fullURL);
 
-        res.render('detail', { view:'item', title, price, unit });
+        const preference = new Preference(client);
 
+        preference.create({
+          body: {
+            items: [
+              {
+                id: Number(1234),
+                title,
+                picture_url: fullURL,
+                description: 'Dispositivo móvil de Tienda e-commerce',
+                quantity: Number(unit),
+                unit_price: Number(price),
+                currency_id: "ARS",
+              }
+            ],
+            payer: {
+              name: 'Lalo',
+              surname: 'Landa',
+              email: 'test_user_36961754@testuser.com',
+              phone: {
+                area_code: '11',
+                number: '2222-3333'
+              },
+              identification: {
+                type: 'DNI',
+                number: '22333444'
+              },
+              address: {
+                street_name: 'calle falsa',
+                street_number: 123,
+                zip_code: '1040'
+              }
+            },
+            back_urls: {
+              success: 'https://mp-ecommerce-nodejs-theta.vercel.app/success',
+              failure: 'https://mp-ecommerce-nodejs-theta.vercel.app/failure',
+              pending: 'https://mp-ecommerce-nodejs-theta.vercel.app/pending'
+            },
+            auto_return: 'approved',
+            payment_methods: {
+            excluded_payment_methods: [
+                      {
+                                id: "visa"
+                      }
+            ],
+            excluded_payment_types: [],
+            installments: 6
+            },
+            notification_url: 'https://mp-ecommerce-nodejs-theta.vercel.app/notifications',
+            statement_descriptor: 'MEUNEGOCIO',
+            external_reference: 'federicomatiasrios@gmail.com',
+          }
+        })
+        .then((preference) => {
+            const initPoint = preference.init_point + "&redirect_mode=modal";
+            console.log('init_point:', initPoint);
+
+            const initPointTry = 'https://www.mercadopago.com.ar/checkout/v1/payment/modal/?preference-id='+preference.id+'&from-widget=true&sniffing-rollout=sniffing-api';
+
+            console.log('preference:', preference);
+
+            //res.redirect(initPoint);
+            res.render('detail', { view:'detail', title, price, unit, preference, initPoint, initPointTry });
+        })
+        .catch((error) => {
+            console.error('Error al crear preferencia:', error);
+            res.status(500).json({ error: 'Error al procesar la solicitud' });
+        });
 
     } catch (error) {
         console.error('Error al procesar la solicitud:', error);
@@ -92,66 +145,4 @@ app.post("/notifications" , function (request, res) {
     res.status(200).send("Ok");
   });
 
-  app.post('/create_preference', function (req, res) {
-
-        let preference = {
-          items: [
-            {
-              id: Number(1234),
-              title: req.body.title,
-              picture_url: ("https://mp-ecommerce-nodejs-theta.vercel.app" + req.body.img),
-              description: 'Dispositivo móvil de Tienda e-commerce',
-              quantity: Number(req.body.unit),
-              unit_price: Number(req.body.price),
-              currency_id: "ARS",
-            }
-          ],
-          payer: {
-            name: 'Lalo',
-            surname: 'Landa',
-            email: 'test_user_36961754@testuser.com',
-            phone: {
-              area_code: '11',
-              number: Number(33670032)
-            },
-            identification: {
-              type: 'DNI',
-              number: '22333444'
-            },
-            address: {
-              street_name: 'calle falsa',
-              street_number: Number(123),
-              zip_code: '1040'
-            }
-          },
-          back_urls: {
-            success: 'https://mp-ecommerce-nodejs-theta.vercel.app/success',
-            failure: 'https://mp-ecommerce-nodejs-theta.vercel.app/failure',
-            pending: 'https://mp-ecommerce-nodejs-theta.vercel.app/pending'
-          },
-          auto_return: 'approved',
-          payment_methods: {
-          excluded_payment_methods: [
-                    {
-                              id: "visa"
-                    }
-          ],
-          excluded_payment_types: [],
-          installments: 6
-          },
-          notification_url: 'https://mp-ecommerce-nodejs-theta.vercel.app/notifications',
-          statement_descriptor: 'MEUNEGOCIO',
-          external_reference: 'federicomatiasrios@gmail.com',
-        };
-    
-        mercadopago.preferences.create(preference)
-              .then(function (response) {
-            let id = response.body.id;
-          res.redirect(response.body.init_point) 
-            })
-            .catch(function (error) {
-                console.log(error);
-              })
-            })
-              
 app.listen(port);
